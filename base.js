659c79ce4010225ba6563fb7acae2f78c6e91337
zhongzhi107 <zhong.zhi@163.com>
2017-04-19 06:33:54 +0800

    增加base基本rules

---
+++ base.js
@@ -0,0 +1,38 @@
+module.exports = {
+  extends: [
+    'eslint-config-airbnb-base',
+    'eslint-config-airbnb-base/rules/strict'
+  ].map(require.resolve),
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "rules": {
+    "import/default": 0,
+    "import/no-duplicates": 0,
+    "import/named": 0,
+    "import/namespace": 0,
+    "import/no-unresolved": 0,
+    "import/no-extraneous-dependencies": 0,
+    "comma-dangle": [2, "never"],
+    "complexity": ["error", { "max": 10 }],
+    "indent": [2, 2, {"SwitchCase": 1}],
+    "no-console": 0,
+    "no-alert": 0,
+    "no-warning-comments": 2,
+    "semi": 2
+  },
+  "plugins": [
+    "import"
+  ],
+  "settings": {
+    "import/parser": "babel-eslint",
+    "import/resolve": {
+      "moduleDirectory": ["node_modules", "src"]
+    }
+  },
+  "globals": {
+    "__": true
+  }
+};
