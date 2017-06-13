d9775100c7021137802475a9340f4ce210f6fc7e
zhongzhi107 <zhong.zhi@163.com>
2017-06-13 18:08:09 +0800

    chore: eslint-config-airbnb@15.0.1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['eslint-config-airbnb', './base'].map(require.resolve),
   plugins: ['react', 'import'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'jsx-a11y/img-has-alt': 0
   }
 };
