708aeac6d479faa42e7fceb5514a62dbf47fcfee
zhongzhi107 <zhong.zhi@163.com>
2018-07-25 15:00:10 +0800

    增加 dace 规则

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 module.exports = {
   extends: ['eslint-config-airbnb', './base'].map(require.resolve),
   plugins: ['react', 'import'],
+  env: {
+    mocha: true
+  },
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-var': 0,
+    'global-require': 0,
+    'react/forbid-prop-types': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'import/no-dynamic-require': 0,
+    'complexity': 0
   }
 };
