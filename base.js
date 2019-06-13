84d75e7db4d4fc151422437b2019ca2c5aa004bf
zhi.zhong <zhong.zhi@163.com>
2019-06-13 12:35:47 +0800

    fix: 允许使用旧的装饰器语法

--- base.js
+++ base.js
@@ -3,39 +3,44 @@ module.exports = {
     'eslint-config-airbnb-base',
     'eslint-config-airbnb-base/rules/strict'
   ].map(require.resolve),
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "node": true
-  },
-  "rules": {
-    "import/default": 0,
-    "import/no-duplicates": 0,
-    "import/named": 0,
-    "import/namespace": 0,
-    "import/no-unresolved": [2, { caseSensitive: true }],
-    "import/no-extraneous-dependencies": 0,
-    "class-methods-use-this": 0,
-    "comma-dangle": [2, "never"],
-    "complexity": ["error", { "max": 10 }],
-    "indent": [2, 2, {"SwitchCase": 1}],
-    "no-alert": 0,
-    "no-console": 0,
-    "no-param-reassign": 0,
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    "no-warning-comments": 2,
-    "semi": 2
-  },
-  "plugins": [
-    "import"
-  ],
-  "settings": {
-    "import/parser": "babel-eslint",
-    "import/resolve": {
-      "moduleDirectory": ["node_modules", "src"]
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      // 允许使用旧的装饰器语法
+      // https://github.com/babel/babel-eslint/issues/662
+      legacyDecorators: true
     }
   },
-  "globals": {
-    "__": true
+  env: {
+    browser: true,
+    node: true
+  },
+  rules: {
+    'import/default': 0,
+    'import/no-duplicates': 0,
+    'import/named': 0,
+    'import/namespace': 0,
+    'import/no-unresolved': [2, { caseSensitive: true }],
+    'import/no-extraneous-dependencies': 0,
+    'class-methods-use-this': 0,
+    'comma-dangle': [2, 'never'],
+    'complexity': ['error', { 'max': 10 }],
+    'indent': [2, 2, {'SwitchCase': 1}],
+    'no-alert': 0,
+    'no-console': 0,
+    'no-param-reassign': 0,
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-warning-comments': 2,
+    'semi': 2
+  },
+  plugins: ['import'],
+  settings: {
+    'import/parser': 'babel-eslint',
+    'import/resolve': {
+      'moduleDirectory': ['node_modules', 'src']
+    }
+  },
+  globals: {
+    '__': true
   }
 };
