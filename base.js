ba4ec796cfb00df9b538a210baec684005fadc70
zhi.zhong <zhong.zhi@163.com>
2019-07-24 16:27:46 +0800

    feat: 增加 typescript 规则文件

--- base.js
+++ base.js
@@ -15,7 +15,17 @@ module.exports = {
     browser: true,
     node: true
   },
+  plugins: ['import', 'filenames'],
+  settings: {
+    'import/parser': 'babel-eslint',
+    'import/resolve': {
+      moduleDirectory: ['node_modules', 'src']
+    }
+  },
   rules: {
+    semi: 2,
+    complexity: 0,
+    indent: [2, 2, { SwitchCase: 1 }],
     'import/default': 0,
     'import/no-duplicates': 0,
     'import/named': 0,
@@ -24,23 +34,13 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'class-methods-use-this': 0,
     'comma-dangle': [2, 'never'],
-    complexity: 0,
-    indent: [2, 2, { SwitchCase: 1 }],
     'no-alert': 0,
     'no-console': 0,
     'no-param-reassign': 0,
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'no-warning-comments': 2,
-    semi: 2
-  },
-  plugins: ['import'],
-  settings: {
-    'import/parser': 'babel-eslint',
-    'import/resolve': {
-      'moduleDirectory': ['node_modules', 'src']
-    }
-  },
-  globals: {
-    '__': true
+    'no-warning-comments': 0,
+    'filenames/match-regex': [2, '^[a-z-]+$', true],
+    'filenames/match-exported': 0,
+    'filenames/no-index': 0
   }
 };
